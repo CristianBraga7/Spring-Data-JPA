4c9af1684764b340395597bffbef88033cabbafc
Cristian Braga <cristihbr@gmail.com>
2025-05-09 17:05:23 -0300

    new

--- libraryapi/src/main/java/com/example/libraryapi/execeptions/RegistroDuplicadoException.java
+++ libraryapi/src/main/java/com/example/libraryapi/execeptions/RegistroDuplicadoException.java
@@ -1,7 +1,6 @@
 package com.example.libraryapi.execeptions;
 
 public class RegistroDuplicadoException extends RuntimeException {
-
     public RegistroDuplicadoException(String message) {
         super(message);
     }
